0cb152aa0e7f325f8d5848e5260f7c223d7d6459
blake-brooks-88 <blakerbrooksbrb@gmail.com>
2025-05-12 19:42:53 -0500

    complete jest setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,9 @@
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function HomeScreen() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-blue-500">
-        Edit app/index.tsx to edit this screen.
-      </Text>
+    <View>
+      <Text>Welcome!</Text>
     </View>
   );
 }
